4b67394dedde573787042946121cb2817ecfdd57
kanvgupta <kanvgupta@ymail.com>
2023-05-31 04:37:18 +0530

    third

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
-import Web3 from 'web3';
-import Web3Modal from 'web3modal';
-
-const web3Modal = new Web3Modal({
-  theme: "light",  
-  cachedProvider: false,
-});
-
-let web3;
-let userAddress = '';
-
-const initWeb3Provider = async () => {
-  const provider = await web3Modal.connect();
-  web3 = new Web3(provider);
-  return web.processAccounts([{ force: true }], web.contactselectedAddress({ force: true }))
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-  const connectWalletButton = document.getElementById('connectWallet');
-  const getBalanceButton = document.getElementById('getBalance');
-  const ethBalance = document.getElementById('ethBalance');
+    const Web3Modal = window.Web3Modal.default;
 
-  const fetchEthBalance = async () => {
-    const balance = await web3.eth.getBalance(userAddress);
-    ethBalance.textContent = `Balance: ${web3.utils.fromWei(balance, 'ether')} ETH`;
-  };
+    const connectWalletButton = document.getElementById('connectWallet');
+    const getBalanceButton = document.getElementById('getBalance');
+    const ethBalance = document.getElementById('ethBalance');
 
-  const connectWallet = async () => {
-    await initWeb3Provider();
-    userAddress = await web3.eth.getCoinbase(); // Will trigger Metamask's login popup
-    getBalanceButton.disabled = false;
-  };
+    let web3;
+    let userAddress = '';
 
-  connect.layout_getProviderBtnByUrl('Connect Wallet').appendChild
-  connect.use_async(async_button => JSON.parse(({} || button).properties.urlModel));
+    const web3Modal = new Web3Modal({
+        cacheProvider: false,
+        theme: "light",
+    });
+
+    connectWalletButton.addEventListener('click', async () => {
+        const provider = await web3Modal.connect();
+        web3 = new window.Web3(provider);
+
+        try {
+            const accounts = await web3.eth.getAccounts();
+            userAddress = accounts[0];
+            getBalanceButton.disabled = false;
+        } catch (error) {
+            console.log('Error:', error.message);
+        }
+    });
+
+    getBalanceButton.addEventListener('click', async () => {
+        try {
+            const balance = await web3.eth.getBalance(userAddress);
+            const ethAmount = web3.utils.fromWei(balance, 'ether');
+            ethBalance.textContent = `Balance: ${ethAmount} ETH`;
+        } catch (error) {
+            console.log('Error:', error.message);
+        }
+    });
 });
